5c07ba6995ba552ff74dfca01c7d89fa1c39f6e6
Carlos <86106523+Csc32@users.noreply.github.com>
2025-09-16 18:11:44 +0000

    feat: add put route to update tasks

--- api/src/routes/routes.js
+++ api/src/routes/routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import {getTasks, setTasks} from "../controllers/taskController.js";
+import {getTasks, setTasks, updateTask} from "../controllers/taskController.js";
 const router = Router();
 
 router.get("/tasks",getTasks);
 router.post("/tasks",setTasks);
+router.put("/tasks/:id",updateTask);
 
 export default router;
 
