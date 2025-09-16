9017f15ed3ea9b698252ab0a7a8e331a7e8d216d
carlos <sanzonettycar@gmail.com>
2025-09-16 05:36:15 -0400

    test database connection

--- api/index.js
+++ api/index.js
@@ -1,11 +1,19 @@
-import express from 'express'
-
+import express from "express";
+import dbPromise from "./src/models/database.js";
 const app = express();
 
 const server = {
-    port: 3000
-} 
-
-app.listen(server.port,(req,res) => {
-    console.log(`Server running at port: ${server.port}`)
-})
+	port: 3000,
+};
+app.get("/", async (req, res) => {
+	try {
+		const db = await dbPromise;
+		const result = await db.all("SELECT * FROM tasks");
+		res.json(result);
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+	}
+});
+app.listen(server.port, (req, res) => {
+	console.log(`Server running at port: ${server.port}`);
+});
