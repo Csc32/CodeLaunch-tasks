cbf254d4d7acd7f5ee2e97df754881609a75ad4e
Carlos <86106523+Csc32@users.noreply.github.com>
2025-09-16 18:47:16 +0000

    feat: add delete route for remove tasks by id

--- api/src/routes/routes.js
+++ api/src/routes/routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import {getTasks, setTasks, updateTask} from "../controllers/taskController.js";
+import {getTasks, removeTask, setTasks, updateTask} from "../controllers/taskController.js";
 const router = Router();
 
 router.get("/tasks",getTasks);
 router.post("/tasks",setTasks);
 router.put("/tasks/:id",updateTask);
+router.delete("/tasks/:id",removeTask);
 
 export default router;
 
