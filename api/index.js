ec80874d68a0157180c1666ef3349fbe93f83274
Carlos <86106523+Csc32@users.noreply.github.com>
2025-09-16 17:39:26 +0000

    refactor: call to app from external app.js file

--- api/index.js
+++ api/index.js
@@ -1,19 +1,8 @@
-import express from "express";
-import dbPromise from "./src/models/database.js";
-const app = express();
-
+import app from "./src/libs/app.js";
 const server = {
 	port: 3000,
 };
-app.get("/", async (req, res) => {
-	try {
-		const db = await dbPromise;
-		const result = await db.all("SELECT * FROM tasks");
-		res.json(result);
-	} catch (err) {
-		res.status(500).json({ error: err.message });
-	}
-});
+
 app.listen(server.port, (req, res) => {
 	console.log(`Server running at port: ${server.port}`);
 });
