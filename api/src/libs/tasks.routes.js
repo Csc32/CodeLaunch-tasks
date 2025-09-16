e6ef4492e9bedd0ac48318e908d3f8e1e3571f0d
Carlos <86106523+Csc32@users.noreply.github.com>
2025-09-16 16:46:44 +0000

    feat: add getTasks function

---
+++ api/src/libs/tasks.routes.js
@@ -0,0 +1,13 @@
+import dbPromise from  "../models/database.js"
+
+
+export async function getTasks(req,res){
+   try {
+		const db = await dbPromise;
+		const result = await db.all("SELECT * FROM tasks");
+        return result.length > 0 ? res.json(result) : res.status(404).json({message: "Tasks Not found"})
+		res.json(result);
+	} catch (err) {
+		return res.status(500).json({ error: err.message });
+	}
+}
