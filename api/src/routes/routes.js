f3b0f7f8ddf860d86b445f12e4fe6f1fd3d12796
carlos <sanzonettycar@gmail.com>
2025-09-18 18:30:18 -0400

    refactor: implement validation middleware in post route

--- api/src/routes/routes.js
+++ api/src/routes/routes.js
@@ -1,12 +1,24 @@
 import { Router } from "express";
-import {getTasks, removeTask, setTasks, updateTask} from "../controllers/taskController.js";
+import {
+	getTasks,
+	removeTask,
+	setTasks,
+	updateTask,
+} from "../controllers/taskController.js";
+import validationMiddleware from "../middlewares/validateMiddleware.js";
 const router = Router();
 
-router.get("/tasks",getTasks);
-router.post("/tasks",setTasks);
-router.put("/tasks/:id",updateTask);
-router.delete("/tasks/:id",removeTask);
+router.get("/tasks", getTasks);
+router.post(
+	"/tasks",
+	validationMiddleware([
+		{ field: "title", rules: ["required", "min:3"] },
+		{ field: "description", rules: ["max:200"] },
+		{ field: "done", rules: ["boolean"] },
+	]),
+	setTasks,
+);
+router.put("/tasks/:id", updateTask);
+router.delete("/tasks/:id", removeTask);
 
 export default router;
-
-
